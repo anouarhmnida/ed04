c5515fe97d321b8a0ee06ad67fa575f06c269b48
dam <2274914@gmail.com>
2024-03-08 20:05:35 +0100

    encapsular

--- src/main/java/org/example/Contacto.java
+++ src/main/java/org/example/Contacto.java
@@ -16,9 +16,9 @@ class Contacto {
     private List<String> phones;
 
     public Contacto(String name, String phone) {
-        this.name = name;
-        this.phones = new ArrayList<>();
-        this.phones.add(phone);
+        this.setName(name);
+        this.setPhones(new ArrayList<>());
+        this.getPhones().add(phone);
     }
 
     public String getName() {
@@ -27,5 +27,13 @@ class Contacto {
 
     public List<String> getPhones() {
         return this.phones;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhones(List<String> phones) {
+        this.phones = phones;
     }
 }
